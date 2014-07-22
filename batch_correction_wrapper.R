bcee88e308275fd5bf6177b6572670246468e8e0
marion landi <marion.landi@clermont.inra.fr>
2014-07-22 17:01:41 +0200

    correcting some errors in source_local() copy bis

--- batch_correction_wrapper.R
+++ batch_correction_wrapper.R
@@ -22,7 +22,7 @@ args = parseCommandArgs(evaluate=FALSE) #interpretation of arguments given in co
 
 source_local <- function(fname){
 	argv <- commandArgs(trailingOnly = FALSE)
-	base_dir <- dirname(substring(argv[grep("--file"=, argv)], 8))
+	base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
 	source(paste(base_dir, fname, sep="/"))
 }
 
